f353b19d10f0e4f6087d20d44bfa2496391fb3ba
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2023-10-24 19:41:04 -0700

    Added playRound function and confirmed it works correctly to output the game result.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 /* Begin with a function called getComputerChoice. 
 
 getComputerChoice()
-
     let choiceArray =[ 
     choice1 = Rock,
     choice2 = Paper,
     choice3 = Scissors,
     ];
 
-    let randomIndex Math.floor(Math.random() * textArray.length)
+    let randomIndex Math.floor(Math.random() * textArray.length) 
+    randomElement = choiceArray[randomeIndex]
 */
 
 function getComputerChoice(){
@@ -22,3 +22,33 @@ function getComputerChoice(){
     let randomElement = choiceArray[randomChoiceIndex]
     return randomElement;
     };
+
+
+function playRound(playerSelection, computerSelection) {
+if( playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    console.log("Draw!")
+    }
+    else if (playerSelection.toLowerCase() === "ROCK" && computerSelection.toLowerCase() === "Paper") {
+        console.log("Paper beats Rock! You lose!")
+    }
+    else if (playerSelection.toLowerCase() === "ROCK" && computerSelection.toLowerCase() === "Scissors") {
+    console.log("Rock beats Scissors! You Win!")
+    }
+    else if (playerSelection.toLowerCase() === "PAPER" && computerSelection.toLowerCase() === "Scissors") {
+    console.log("Scissors beats Paper! You Lose!")
+    }
+    else if (playerSelection.toLowerCase() === "PAPER" && computerSelection.toLowerCase() === "Rock") {
+    console.log("Scissors beats Paper! You Lose!")
+    }
+    else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Rock") {
+    console.log("Scissors beats Paper! You Lose!")
+    }
+    else{
+    console.log("Scissors beats Paper! You Lose!")
+    }
+}
+
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+
